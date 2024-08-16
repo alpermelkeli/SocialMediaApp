a6e8c719838e42e8086e82fc9d67ac65d5cdb856
alpermelkeli <alpermelkeli@gmail.com>
2024-08-16 18:48:22 +0300

    New page added search.

--- app/src/main/java/com/alpermelkeli/socialmediaapp/navigation/HomeRoutes.kt
+++ app/src/main/java/com/alpermelkeli/socialmediaapp/navigation/HomeRoutes.kt
@@ -4,6 +4,7 @@ import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.alpermelkeli.socialmediaapp.R
 
@@ -13,7 +14,8 @@ sealed class HomeRoutes(val route:String,
     ){
 
     object HomePage : HomeRoutes("homepage", R.string.home , Icons.Default.Home)
+    object Search : HomeRoutes("search",R.string.search, Icons.Default.Search)
     object Profile : HomeRoutes("profile", R.string.profile, Icons.Default.Person)
 
 }
-val homeRoutes = listOf(HomeRoutes.HomePage, HomeRoutes.Profile)
+val homeRoutes = listOf(HomeRoutes.HomePage, HomeRoutes.Search, HomeRoutes.Profile)
--- app/src/main/java/com/alpermelkeli/socialmediaapp/screens/Profile.kt
+++ app/src/main/java/com/alpermelkeli/socialmediaapp/screens/Profile.kt
@@ -1,16 +1,72 @@
 package com.alpermelkeli.socialmediaapp.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.alpermelkeli.socialmediaapp.R
+import com.alpermelkeli.socialmediaapp.components.ProfileTopBar
 import com.alpermelkeli.socialmediaapp.repository.AuthOperations
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun Profile(onClickedLogOut:()->Unit){
-    Button(onClick = {AuthOperations.logOut()
-        onClickedLogOut()
-    }) {
+    Scaffold(Modifier.fillMaxSize(),
+        topBar = {ProfileTopBar(onMenuClicked = {}
+        )}) {
+        Box(Modifier.fillMaxSize().padding(start = 10.dp, top = 90.dp),
+            contentAlignment = Alignment.TopStart) {
+            Image(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "",
+                modifier = Modifier.clip(CircleShape)
+                    .size(90.dp)
+            )
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .height(220.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
 
+
+
+
+        }
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ProfilePreview(){
+    Profile {
 
     }
 }
+/*Button(onClick = {AuthOperations.logOut()
+                    onClickedLogOut()
+}) {
+
+
+}
+ */
