41b26c8df5c534cfbdf9283e28349c62df9af867
alpermelkeli <alpermelkeli@gmail.com>
2024-08-17 11:38:32 +0300

    Problem solved by save username and profile photo to posts collection.

--- app/src/main/java/com/alpermelkeli/socialmediaapp/repository/PostsRepository.kt
+++ app/src/main/java/com/alpermelkeli/socialmediaapp/repository/PostsRepository.kt
@@ -2,9 +2,9 @@ package com.alpermelkeli.socialmediaapp.repository
 
 import com.alpermelkeli.socialmediaapp.model.Post
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.coroutines.runBlocking
 
-class PostsRepository(private val userRepository: UserRepository) {
+class PostsRepository {
+
     private val db = FirebaseFirestore.getInstance()
 
     fun getUserHomePagePosts(followings: List<String>, callBack: (List<Post>) -> Unit) {
@@ -14,26 +14,22 @@ class PostsRepository(private val userRepository: UserRepository) {
         }
 
         db.collection("Posts")
-            .whereIn("sender", followings) // Filter posts whose sender id is in the followings list
+            .whereIn("sender", followings) // Filter posts whose id is in the followings list
             .get()
             .addOnSuccessListener { querySnapshot ->
                 val posts = querySnapshot.documents.mapNotNull { document ->
                     val comments = document.get("comments") as? List<String> ?: emptyList()
                     val id = document.id
-                    val sender = document.getString("sender") ?: "Error"
                     val images = document.get("images") as? List<String> ?: emptyList()
                     val likeCount = document.getLong("likeCount")?.toInt() ?: 100
-
-                    val user = runBlocking {
-                        userRepository.getUserDocumentAsync(sender)
-                    }
-
-                    Post(id,comments,likeCount,images,user!!)
-
+                    val username = document.getString("username") ?: ""
+                    val profilePhoto = document.getString("profilePhoto") ?: ""
+                    Post(id,comments,likeCount,images,profilePhoto,username)
                 }
                 callBack(posts)
             }
             .addOnFailureListener { exception ->
+                // Handle any errors here
                 callBack(emptyList())
             }
     }
