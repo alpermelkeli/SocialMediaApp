7c9162709eeb47aaed0821c4a244a321a59d8c2a
VroomPsshh <siddhantgauttam@gmail.com>
2024-08-20 00:08:13 +0530

    image Capturing using camera

--- app/src/main/java/com/alpermelkeli/socialmediaapp/screens/Search.kt
+++ app/src/main/java/com/alpermelkeli/socialmediaapp/screens/Search.kt
@@ -1,11 +1,8 @@
 package com.alpermelkeli.socialmediaapp.screens
-
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview
 @Composable
 fun Search() {
-
-
 }
